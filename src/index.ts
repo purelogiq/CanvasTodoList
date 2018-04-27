3bdb68b5bb028cd5f9a06df1b81d39983c5f61a0
Israel Madueme <purelogiq@gmail.com>
2018-04-27 04:16:24 -0400

    Add pixi.js and initial canvas (#6)

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
-function greeter(person: string) {
-  return `Hello, ${person}`;
+import './.lib/pixi.js';
+
+let app = null;
+
+function init() {
+  app = new PIXI.Application({
+    width: document.body.clientWidth,
+    height: document.body.clientHeight,
+    backgroundColor: 0x330099
+  });
+
+  let circles = PIXI.Sprite.fromImage('images/circles.png');
+  circles.anchor.set(0.5);
+  circles.x = app.screen.width / 2;
+  circles.y = app.screen.height / 2;
+  app.stage.addChild(circles);
+  app.ticker.add(function(delta) {
+    circles.rotation += 0.01 * delta;
+  });
+
+  document.body.appendChild(app.view);
 }
 
-let user = 'Hi momssd';
-
-document.body.innerHTML = greeter(user);
+window.addEventListener('load', init);
